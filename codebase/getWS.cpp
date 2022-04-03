19e5c77e6a58fc6d5d6993f53eb7d5ddd01cbdd1
Troy Gu (MacOS) <troygu@troys-mbp.wifi.local.cmu.edu>
2022-04-03 13:46:00 -0400

    getWS() initial commit.

---
+++ codebase/getWS.cpp
@@ -0,0 +1,195 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <stdio.h>
+#include <thread>
+#include <future>
+#include <condition_variable>
+#include <mutex>
+#include <functional>
+#include <string>
+// #include <Eigen/Dense>
+
+class Robot
+{
+private:
+    class Joint;
+    std::vector<std::vector<Joint>> joints; // a vector of joints from base to end-effector
+public:
+    // void read_urdf(filepath);
+    std::vector<std::vector<double>> forward_kinematics(std::vector<std::vector<double>> joint_settings) const;
+    void getWS(std::vector<std::vector<double>> configs, const int nThreads) const;
+    // unordered_map point_cloud;
+    // void save2pcd();
+};
+
+class Robot::Joint
+{
+    typedef enum
+    {
+        revolute,
+        prismatic
+    } joint_type;
+    std::string frame_name;
+    std::string parent_frame;
+    // Eigen::Vector3d origin_xyz; // offset from parent frame
+    // Eigen::Vector3d origin_rpy;
+    // Eigen::Vector3d axis;
+    double joint_state;
+    double joint_limit[2];
+    // rotational axis
+    // joint angle
+    // lower and upper limit
+};
+
+std::vector<std::vector<double>> Robot::forward_kinematics(std::vector<std::vector<double>> configs) const
+{
+    /*
+    summary: the function takes in a batch of configurations and computes corresponding end-effector positions
+    param configs:
+        1st dim - an array (vector) of different configurations;
+                  dim1 = number of configs
+        2nd dim - an array (vector) of settings (i.e. length/angle) for each Joint (idx_0 is base and idx_end is end-effector);
+                  dim2 = joints.size()
+    return: a 2d vector 
+        1st dim - various configurations
+        2nd dim - xyz cooridnates
+    */
+}
+
+// =================================== MULTITHREADING ===========================================
+class WorkerThread
+{
+private:
+    enum
+    {
+        TASK_NONE,
+        TASK_QUIT,
+        TASK_RUN
+    };
+
+    int taskType = TASK_NONE;
+    std::function<void()> task;
+    std::thread thr;
+    std::mutex mtx;
+    std::condition_variable cond;
+    void ThreadFunc();
+
+public:
+    WorkerThread();
+    ~WorkerThread();
+    void Run(std::function<void()>);
+    void Wait(void);
+};
+
+// constructor; on main thread; prestart the threads
+WorkerThread::WorkerThread()
+{
+    std::thread t(&WorkerThread::ThreadFunc, this);
+    thr.swap(t);
+}
+// destructor; on main thread
+WorkerThread::~WorkerThread()
+{
+    taskType = TASK_QUIT;
+    cond.notify_one();
+    thr.join();
+}
+void WorkerThread::Run(std::function<void()> newTask)
+{
+    mtx.lock();
+    taskType = TASK_RUN;
+    task = newTask;
+    mtx.unlock();
+    cond.notify_one();
+}
+void WorkerThread::Wait(void)
+{
+    std::unique_lock<std::mutex> lock(mtx);
+    cond.wait(lock, [&]{ return taskType == TASK_NONE; });
+}
+
+void WorkerThread::ThreadFunc()
+{
+    for (;;)
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        cond.wait(lock, [&]{ return taskType != TASK_NONE; });
+        if (TASK_QUIT == taskType)
+        {
+            break;
+        }
+        else if (TASK_RUN == taskType)
+        {
+            task();
+            taskType = TASK_NONE;
+            cond.notify_one();
+        }
+    }
+}
+
+// =================================== MULTITHREADING ===========================================
+
+// =================================== VERSION 1 ===========================================
+// void Robot::getWS(std::vector<std::vector<double>> configs, const int nThreads) const
+// {
+//     /*
+//     param configs: a 2D array (vector) that stores all possible configurations
+//     param nThreads: number of threads available
+//     */
+//     int num_jobs = configs.size();
+//     WorkerThread *subThr = new WorkerThread[nThreads];
+//     std::vector<std::vector<double>> all_results;
+//     for (int i = 0; i < nThreads; ++i)
+//     {
+//         // slice the vector
+//         auto start = configs.begin() + i*(num_jobs / nThreads);
+//         auto end = configs.begin() + (i+1)*(num_jobs / nThreads); // not including the last element
+//         (end >= configs.end()) ? end = configs.end();
+//         auto some_configs = std::vector<std::vector<double>>(start, end);
+//         // feed to threads
+//         std::vector<double> results;
+//         subThr[i].Run([&]{ results = forward_kinematics(some_configs); });
+//         all_results.push_back(results);
+//     }
+//     for (int i = 0; i < nThreads; ++i)
+//     {
+//         subThr[i].Wait();
+//     }
+//     delete[] subThr;
+//     subThr = nullptr;
+// }
+
+// =================================== VERSION 2 ===========================================
+void Robot::getWS(std::vector<std::vector<double>> configs, const int nThreads) const
+{
+    /*
+    param configs: a 2D array (vector) that stores all possible configurations
+    param nThreads: number of threads available
+    */
+    int num_jobs = configs.size();
+    // WorkerThread *subThr = new WorkerThread[nThreads];
+    std::vector<std::vector<double>> all_results;
+    for (int i = 0; i < nThreads; ++i)
+    {
+        // slice the vector
+        auto start = configs.begin() + i*(num_jobs / nThreads);
+        auto end = configs.begin() + (i+1)*(num_jobs / nThreads); // not including the last element
+        (end >= configs.end()) ? end = configs.end();
+        auto some_configs = std::vector<std::vector<double>>(start, end); // same type as configs
+        // feed to threads
+        auto fut = std::async(std::launch::async, &Robot::forward_kinematics, this, some_configs);
+        // typecast future objects; a vector of xyz coordinates
+        std::vector<std::vector<double>> thread_res = fut.get();
+        // insert thread_results into all_results
+        all_results.insert(all_results.end(), 
+                        std::make_move_iterator(thread_res.begin()), 
+                        std::make_move_iterator(thread_res.end()));
+    }
+    // for (int i = 0; i < nThreads; ++i)
+    // {
+    //     subThr[i].Wait();
+    // }
+    // delete[] subThr;
+    // subThr = nullptr;
+}
