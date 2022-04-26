17f5ed8dbd1a0db7306dcbfe510b0a565dd72301
DengYuelin <dengyuelin66dyl@163.com>
2022-04-26 00:31:50 -0400

    fk tested at one point

--- codebase/Beta/tests/test_fk1.cpp
+++ codebase/Beta/tests/test_fk1.cpp
@@ -1,6 +1,32 @@
 #include "Robot.h"
 
+const double epsilon = 0.00001;
+
+bool approximatelyEqual(double a, double b, double epsilon) {
+  return std::fabs(a - b) <=
+         ((std::fabs(a) < std::fabs(b) ? std::fabs(b) : std::fabs(a)) *
+          epsilon);
+}
+
 int main() {
   Robot robot;
-  return 1;
+  if (0 != robot.read_urdf("../../NamingSoHard/codebase/Beta/tests/ur5.urdf")) {
+    return 1;
+  }
+  std::vector<double> joint_angles;
+  joint_angles.clear();
+  joint_angles.push_back(0);
+  joint_angles.push_back(0);
+  joint_angles.push_back(0);
+  joint_angles.push_back(0);
+  joint_angles.push_back(0);
+  joint_angles.push_back(0);
+  auto result = robot.forward_kinematics_single(joint_angles);
+
+  if (approximatelyEqual(result(0), 0, epsilon) &&
+      approximatelyEqual(result(1), 0.19145, epsilon) &&
+      approximatelyEqual(result(2), 1.00106, epsilon))
+    return 0;
+  else
+    return 2;
 }
